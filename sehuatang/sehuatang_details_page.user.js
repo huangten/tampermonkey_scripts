990dbe197259a2ee93fe11863c15705ca18e3890
dailipeng <1822185693@qq.com>
2024-10-25 13:26:14 +0800

    update

--- sehuatang/sehuatang_details_page.user.js
+++ sehuatang/sehuatang_details_page.user.js
@@ -250,7 +250,7 @@
 
         function getSelfFilename(el) {
             let title = getTitleText(el);
-            let replaceList = '/?*:|\<>"'.split('_');
+            let replaceList = '/?*:|\<>"'.split('');
             let equalList = ["con", "aux", "nul", "prn", "com0", "com1", "com2", "com3", "com4", "com5", "com6", "com7",
                 "com8", "com9", "lpt0", "lpt1", "lpt2", "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9"];
 
