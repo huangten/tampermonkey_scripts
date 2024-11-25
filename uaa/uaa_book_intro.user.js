f624a53aa343e286ff6c39795ab00fcde8a90728
dailipeng <1822185693@qq.com>
2024-11-26 01:57:19 +0800

    update

--- uaa/uaa_book_intro.user.js
+++ uaa/uaa_book_intro.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         uaa 详情页相关操作
 // @namespace    http://tampermonkey.net/
-// @version      2024-11-25
+// @version      2024-11-26
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.uaa.com/novel/intro*
@@ -277,7 +277,7 @@
                 }
                 timer = setTimeout(() => {
                     doDownload()
-                }, 1000 * 5);
+                }, 1000 * 3);
             }
         }
         function cycleClear(el) {
