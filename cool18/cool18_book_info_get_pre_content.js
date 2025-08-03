fe8d06647637c09593657afb17da3d3a1435be83
dailipeng <1822185693@qq.com>
2025-08-03 23:16:37 +0800

    update

--- cool18/cool18_book_info_get_pre_content.js
+++ cool18/cool18_book_info_get_pre_content.js
@@ -1,21 +1,17 @@
 // ==UserScript==
 // @name         获取禁忌书屋书籍内容
 // @namespace    http://tampermonkey.net/
-// @version      2025-08-03.4
+// @version      2025-08-03.5
 // @description  try to take over the world!
 // @author       You
 // @match        *://www.cool18.com/bbs4/index.php?app=forum&act=threadview&tid=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=cool18.com
-// @grant        GM_xmlhttpRequest
 // @connect      *
-// @grant        GM_download
 // @grant        unsafeWindow
 // ==/UserScript==
 
 (function () {
     'use strict';
-
-
 
     function addCss(id, src) {
         return new Promise((resolve, reject) => {
