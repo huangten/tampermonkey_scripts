d83eb6d37456b7174e4af45a2e300d83ad5962d4
dailipeng <1822185693@qq.com>
2026-01-07 21:14:01 +0800

    更新 sis001 并去掉 sis001_book_chapter.user.js 脚本

---
+++ src/sis001/chapter/chapter.js
@@ -0,0 +1,86 @@
+import {addCss, copyContext, addScript} from '../../common/common.js'
+
+Promise.all([
+    addCss('layui_css', 'https://cdn.jsdelivr.net/npm/layui@2.11.5/dist/css/layui.min.css'),
+    // addScript('filesave_id', "https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js"),
+    addScript('layui_id', "https://cdn.jsdelivr.net/npm/layui@2.11.5/dist/layui.min.js")
+]).then(() => {
+    run();
+});
+
+function run() {
+    layui.use(function () {
+        const util = layui.util;
+        const fixbarStyle = "background-color: #ba350f;font-size: 16px;width:160px;height:36px;line-height:36px;margin-bottom:6px;border-radius:10px;"
+        // 自定义固定条
+        util.fixbar({
+            bars: [
+                {
+                    type: 'CopyContent',
+                    content: '复制内容',
+                    style: fixbarStyle
+                },
+                {
+                    type: 'CopyContentHtml',
+                    content: '复制内容HTML',
+                    style: fixbarStyle
+                }
+            ],
+            default: false,
+            css: {bottom: "21%"},
+            margin: 0,
+            // 点击事件
+            click: function (type) {
+                console.log(this, type);
+                // layer.msg(type);
+                if (type === "CopyContent") {
+                    getPreTagContent();
+
+                }
+                if (type === "CopyContentHtml") {
+                    getPreTagContentHtml();
+                }
+
+            }
+        });
+    });
+}
+
+function getElement(uw) {
+    let e = uw.document.getElementsByClassName('postcontent')[0].getElementsByClassName("postmessage")[0];
+
+    let e1 = e.cloneNode(true);
+
+    let fieldset = e1.getElementsByTagName("fieldset")[0];
+    let table = e1.getElementsByTagName("table")[0];
+
+
+    // 1. 选中所有后代元素（包括直接子元素和非直接子元素）
+    const allDescendants = e1.querySelectorAll('*');
+
+    // 2. 遍历所有后代元素
+    allDescendants.forEach(element => {
+        // 3. 判断元素的直接父元素是否就是目标父元素
+        if (element === fieldset) {
+            // 4. 如果不是，则它是非直接子元素（孙子、曾孙等），执行移除
+            element.remove();
+        }
+        if (element === table) {
+            // 4. 如果不是，则它是非直接子元素（孙子、曾孙等），执行移除
+            element.remove();
+        }
+        if (element.tagName === "A") {
+            element.remove();
+        }
+    });
+
+    return e1;
+}
+
+function getPreTagContent(uw = unsafeWindow) {
+    copyContext(getElement(uw).innerText).then();
+}
+
+function getPreTagContentHtml(uw = unsafeWindow) {
+    copyContext(getElement(uw).innerHTML).then();
+}
---
+++ vite.config.sis001.js
@@ -0,0 +1,75 @@
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+import monkey, {util} from 'vite-plugin-monkey';
+import AutoImport from 'unplugin-auto-import/vite';
+
+
+const date = new Date();
+const version = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}.01`
+
+// 1. 定义你的脚本库配置
+const scriptConfigs = {
+    sis001_chapter: {
+        entry: 'src/sis001/chapter/chapter.js',
+        userscript: {
+            name: 'sis001 章节页 增强',
+            author: 'YourName',
+            match: [
+                '*://*.sis001.com/forum/thread-*-1-1.html',
+                '*://*.sis001.com/bbs/viewthread.php?tid=*',
+                '*://*.sis001.com/bbs/thread-*-1-1.html'
+            ],
+            namespace: 'https://tampermonkey.net/',
+            version: version,
+            noframes: true,
+        },
+        build: {
+            outDir: "sis001",
+            fileName: "sis001_book_chapter"
+        }
+    }
+};
+
+
+export default defineConfig(({mode}) => {
+
+    console.log(mode)
+    // 2. 获取当前要打包的脚本 Key
+    const targetKey = process.env.TARGET_SCRIPT;
+    const config = scriptConfigs[targetKey];
+    if (!config) {
+        throw new Error(`请指定有效的脚本名称！当前输入: ${targetKey}`);
+    }
+
+    return {
+        build: {
+            outDir: `dist/${config.build.outDir}`,
+            emptyOutDir: false
+        },
+        server: {
+            open: false,
+            port: 5173,
+            strictPort: true, // 如果 5173 被占用直接报错，而不是切换到 5173
+        },
+        plugins: [
+            vue(),
+            AutoImport({
+                imports: [util.unimportPreset],
+            }),
+            monkey(
+                {
+                    entry: config.entry,
+                    userscript: {
+                        grant: ['GM_getResourceText', 'GM_addStyle', 'unsafeWindow'],
+                        ...config.userscript
+                    },
+                    build: {
+                        // 3. 关键：让生成的脚本文件名包含脚本 Key
+                        fileName: `${config.build.fileName}.user.js`,
+                    }
+                }),
+        ],
+    };
+});
+
+
