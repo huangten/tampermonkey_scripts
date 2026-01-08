db73ca970dec3b0ed7b026306deb94548cbe83b3
dailipeng <1822185693@qq.com>
2026-01-08 17:24:18 +0800

    update

--- src/uaa/list/Download.js
+++ src/uaa/list/Download.js
@@ -204,7 +204,7 @@ async function buildEpub(url) {
             }
         }
 
-console.log(doc)
+
         let chapters = getChapterMenu(doc)
 
         zip.file('mimetype', 'application/epub+zip', {compression: 'STORE'});
@@ -352,7 +352,7 @@ ${ncxNav.join('\n')}
 </navMap>
 </ncx>`;
         o.file('toc.ncx', formatXML(tocNcxStr));
-
+console.log(contentOpfStr)
         const blob = await zip.generateAsync({type: 'blob'});
         saveAs(blob, `${bookName} 作者：${author}.epub`);
         console.log(bookName + ' 下载完毕！');
