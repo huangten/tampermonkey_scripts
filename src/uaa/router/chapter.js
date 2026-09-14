369a09c21337a11a88673cfe82384a8d9416f0ee
dailipeng <1822185693@qq.com>
2026-09-14 21:46:23 +0800

    去除 UAA 多余的脚本.

--- src/uaa/router/chapter.js
+++
@@ -1,8 +0,0 @@
-import { init } from "../../common/common.js";
-import { ChapterController } from "../controllers/ChapterController.js";
-
-init().then(() => {
-    new ChapterController().init();
-}).catch((e) => {
-    console.log(e);
-});
--- src/uaa/router/introV3.js
+++
@@ -1,8 +0,0 @@
-import { init } from "../../common/common.js";
-import { IntroV3Controller } from "../controllers/IntroV3Controller.js";
-
-init().then(async () => {
-    await new IntroV3Controller().init();
-}).catch((e) => {
-    console.log(e);
-});
