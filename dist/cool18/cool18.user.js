3959281c6e50d32fb01726c9febec360fb320761
dailipeng <1822185693@qq.com>
2026-09-17 15:39:56 +0800

    cool18 update monaca editor.

--- dist/cool18/cool18.user.js
+++ dist/cool18/cool18.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       cool18 增强
 // @namespace  https://tampermonkey.net/
-// @version    2026-09-17.15:37:55
+// @version    2026-09-17.15:39:49
 // @author     YourName
 // @icon       https://www.google.com/s2/favicons?sz=64&domain=cool18.com
 // @match      *://*.cool18.com/*
