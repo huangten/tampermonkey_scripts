1185d25f30c84dd7fb95785dd24c495c982ac270
dailipeng <1822185693@qq.com>
2026-09-17 14:14:12 +0800

    cool18 update monaca editor.

--- vite.config.cool18.js
+++ vite.config.cool18.js
@@ -59,7 +59,9 @@ export default defineConfig(({mode}) => {
                     entry: config.entry,
                     userscript: {
                         require: [
-                            'https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js'
+                            'https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js',
+                            'https://cdn.jsdelivr.net/npm/monaco-editor@0.56.0/min/vs/editor/editor.main.min.js',
+                            // 'https://jsdelivr.net'
                         ],
                         grant: ['GM_getResourceText', 'GM_addStyle', 'unsafeWindow', 'GM_notification', 'GM_download'],
                         ...config.userscript
